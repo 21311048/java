528ee9eac6ca64b011f51fadd26d294f866d5dfa
21311048 <21311048te@tama.ac.jp>
2014-10-22 15:27:29 +0900

    第３講義計算のプログラム

--- keisan.java
+++ keisan.java
@@ -2,44 +2,25 @@ class keisan
 {
 	public static void main(String argv[])
 	{
-		int number1,number2,answer;
-		String kigou;
+		int number1,number2,tasu,hiku,kakeru,waru,amari;
 		
 		System.out.println("計算を行います、計算したい数値1を入力してください。");
 		number1 = new java.util.Scanner(System.in).nextInt();
-		System.out.println("計算を行う際の計算方法を、+ - * / %で入力してください。");
-		kigou = new java.util.Scanner(System.in).nextLine(); 
 		System.out.println("計算したい数値2を入力してください。");
 		number2 = new java.util.Scanner(System.in).nextInt();
 		
-		if(kigou.equals("+"))
-		{
-			answer = number1 + number2;
-			System.out.println(number1+"＋"+number2+"は"+answer+"です。");
-		}
-		else if(kigou.equals("-"))
-		{
-			answer = number1 - number2;
-			System.out.println(number1+"－"+number2+"は"+answer+"です。");
-		}
-		else if(kigou.equals("*"))
-		{
-			answer = number1 * number2;
-			System.out.println(number1+"×"+number2+"は"+answer+"です。");
-		}
-		else if(kigou.equals("*"))
-		{
-			answer = number1 / number2;
-			System.out.println(number1+"÷"+number2+"は"+answer+"です。");
-		}
-		else if(kigou.equals("%"))
-		{
-			answer = number1 % number2;
-			System.out.println(number1+"と"+number2+"の商の余りは"+answer+"です。");
-		}
-		else
-		{
-			System.out.print("+ - * / %のいずれかを入力してください。");
-		}
-	}
+		tasu = number1 + number2;
+		hiku = number1 - number2;
+		kakeru = number1 * number2;
+		waru = number1 / number2;
+		amari = number1 % number2;
+		
+		
+		System.out.println("数値1+数値2は"+tasu+"です。");
+		System.out.println("数値1-数値2は"+hiku+"です。");
+		System.out.println("数値1×数値2は"+kakeru+"です。");
+		System.out.println("数値1÷数値2は"+waru+"です。");
+		System.out.println("数値1から数値2を割った余りは"+amari+"です。");
+	}	
+		
 }
