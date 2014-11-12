b73ed149a81ebf778471e61cff490bfbfce9cd1b
21311048 <21311048te@tama.ac.jp>
2014-11-12 15:03:32 +0900

    前回のリュカ数列プログラムの修正版、BigIntegerを用いて正しい数を求められるように変更。

--- Lucas.java
+++ Lucas.java
@@ -1,27 +1,30 @@
+import java.math.*;
+
 class Lucas
 {
 	public static void main(String argv[])
 	{
-		long[] x = new long[101];
-		long goukei;
-		goukei = 0;
+		BigInteger[] x = new BigInteger[101];
+		BigInteger goukei,bigi,bigb;
+		goukei = new BigInteger("0");
 		for(int i=1; i<=100; i++)
 		{
 			if(i==1){
-				x[i] = 1;}
-			else if(i==2){		
-				x[i] = 3;}
+				bigi = BigInteger.valueOf(i); 
+				x[i] = new BigInteger("1");}
+			else if(i==2){	
+				bigi = BigInteger.valueOf(i); 
+				x[i] = new BigInteger("3");}
 			else{
-				x[i] = x[i-1] + x[i-2];}
+				bigi = BigInteger.valueOf(i); 
+				x[i] = x[i-1].add(x[i-2]);}
 		}	
 
 		for(int b=1; b<=100; b++)
 		{
-			goukei = goukei + x[b];
+			bigb = BigInteger.valueOf(b); 
+			goukei = goukei.add(x[b]);
 		}	
 		System.out.println(goukei);
 	}
 }
-//long型の変数で参照可能な数値は「9223372036854775807」である
-//しかし、今回のプログラムで求める数は22桁と非常に多い数であるため、intやlong型だとオーバーフローを起こす。
-//そのため、long型より多くの数を参照できるデータ型が必要である。
